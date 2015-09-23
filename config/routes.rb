9d90065a4c5fbd4132a4d37dfd10cb1be2df1166
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-23 09:52:57 -0600

    Root page setup w/ login modal & flash errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'sessions#create'
+  root 'welcome#index'
 
   resources :users
+
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
